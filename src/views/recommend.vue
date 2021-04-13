31d4d5cb9586700e87253970b62dc0dfc01e48bf
ldh <627247084@qq.com>
2021-04-14 00:44:49 +0800

    歌手页面开发

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="recommend">
+  <div class="recommend" v-loading="loading">
     <scroll class="recommend-content">
       <div>
         <div class="slider-wrapper">
@@ -8,11 +8,11 @@
           </div>
         </div>
         <div class="recommend-list">
-          <h1 class="list-title">热门歌单推荐</h1>
+          <h1 class="list-title" v-show="!loading">热门歌单推荐</h1>
           <ul>
             <li v-for="item in albums" :key="item.id" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.pic" alt="" />
+                <img width="60" height="60" v-lazy="item.pic" alt="" />
               </div>
               <div class="text">
                 <h2 class="name">
@@ -44,6 +44,11 @@ export default {
     return {
       sliders: [],
       albums: []
+    }
+  },
+  computed: {
+    loading() {
+      return !this.sliders.length && !this.albums.length
     }
   },
   async created() {
--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="singer">
-    歌手页面
+  <div class="singer" v-loading="!singers.length">
+    <index-list :data="singers"></index-list>
   </div>
 </template>
 
 <script>
+import { getSingerList } from '@/service/singer'
+import IndexList from '@/components/base/index-list/index-list'
 export default {
   name: 'singer',
+  components: {
+    IndexList
+  },
   data() {
-    return {}
+    return { singers: [] }
+  },
+  async created() {
+    const result = await getSingerList()
+    this.singers = result.singers
   },
   methods: {}
 }
 </script>
 
-<style scoped lang="scss" rel="stylesheet/scss"></style>
+<style scoped lang="scss" rel="stylesheet/scss">
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
+</style>
